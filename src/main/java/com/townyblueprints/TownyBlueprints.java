fdb2cb7cef9c645aa5d6d11b47aa65f81e3b5bf3
ParvatisGitHub <98566804+ParvatisGitHub@users.noreply.github.com>
2025-04-06 22:06:35 -0400

    updated checks on soft depends

--- src/main/java/com/townyblueprints/TownyBlueprints.java
+++ src/main/java/com/townyblueprints/TownyBlueprints.java
@@ -17,7 +17,10 @@ import com.townyblueprints.visualization.ExistingBlueprintVisualizer;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.dynmap.markers.MarkerAPI;
 
 @Getter
 public class TownyBlueprints extends JavaPlugin {
@@ -37,6 +40,12 @@ public class TownyBlueprints extends JavaPlugin {
     private BlockDefinitionManager blockDefinitionManager;
     private ToolDefinitionManager toolDefinitionManager;
     private IStorage database;
+    private static boolean dynmapTownyInstalled;
+    private static boolean slimeFunInstalled;
+    private static boolean mythicMobsInstalled;
+    private static boolean mmmoItemsInstalled;
+    private static boolean itemsAdderInstalled;
+    private static boolean oraxenInstalled;
 
     @Override
     public void onEnable() {
@@ -66,6 +75,9 @@ public class TownyBlueprints extends JavaPlugin {
         this.upkeepHandler = new UpkeepHandler(this);
         this.blockDefinitionManager = new BlockDefinitionManager(this);
         this.toolDefinitionManager = new ToolDefinitionManager(this);
+
+        // Setup integrations with other plugins
+        setupIntegrationsWithOtherPlugins();
 
         // Load configuration and data
         this.configManager.loadConfig();
@@ -103,12 +115,60 @@ public class TownyBlueprints extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(this.resourceCollectionHandler, this);
         this.getServer().getPluginManager().registerEvents(new PlayerMovementListener(this), this);
         this.getServer().getPluginManager().registerEvents(new TownStatusListener(this), this);
-
+        if(dynmapTownyInstalled) {
+            MarkerAPI markerAPI = (MarkerAPI) Bukkit.getPluginManager().getPlugin("Dynmap");
+            if (markerAPI != null) {
+                this.getServer().getPluginManager().registerEvents(new DynmapListener(this, markerAPI), this);
+            }
+        }
         // Start blueprint status check based on configuration
         int interval = getConfig().getInt("blueprints.status_check.interval", 100);
         new BlueprintStatusTask(this).runTaskTimer(this, interval, interval);
 
         this.getLogger().info("TownyBlueprints has been enabled!");
+    }
+
+    private void setupIntegrationsWithOtherPlugins() {
+        // Check for Dynmap-Towny
+        Plugin dynmapTowny = Bukkit.getPluginManager().getPlugin("Dynmap-Towny");
+        dynmapTownyInstalled = dynmapTowny != null;
+        if(dynmapTownyInstalled)
+            info("  DynmapTowny Integration Enabled");
+        Plugin slimeFun = Bukkit.getPluginManager().getPlugin("Slimefun");
+        slimeFunInstalled = slimeFun != null;
+        if(slimeFunInstalled)
+            info("  Slimefun Integration Enabled");
+
+        Plugin mythicMobs = Bukkit.getPluginManager().getPlugin("MythicMobs");
+        if(mythicMobs != null) {
+            String className = Bukkit.getServer().getPluginManager().getPlugin("MythicMobs").getClass().getName();
+            if (className.equals("io.lumine.mythic.bukkit.MythicBukkit")) {
+                mythicMobsInstalled = true;
+                info("  Mythic Mobs Integration Enabled");
+            } else {
+                mythicMobsInstalled = false;
+                this.getLogger().severe("Problem enabling mythic mobs");
+            }
+        }
+
+        Plugin itemsAdder = Bukkit.getPluginManager().getPlugin("ItemsAdder");
+        itemsAdderInstalled = itemsAdder != null;
+        if (itemsAdderInstalled)
+            info("  ItemsAdder Integration Enabled");
+
+        Plugin oraxen = Bukkit.getPluginManager().getPlugin("Oraxen");
+        oraxenInstalled = oraxen != null;
+        if (oraxenInstalled)
+            info("  Oraxen Integration Enabled");
+
+        Plugin mmmoItems = Bukkit.getPluginManager().getPlugin("MMOItems");
+        mmmoItemsInstalled = mmmoItems != null;
+        if (mmmoItemsInstalled)
+            info("  MMOItems Integration Enabled");
+    }
+
+    public static void info(String message) {
+        TownyBlueprints.getInstance().getLogger().info(message);
     }
 
     @Override
